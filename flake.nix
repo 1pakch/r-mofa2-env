c399bf8abad6657c6e27c2934ad7127b34830e2b
Ilya Kolpakov <ikolpakov@health2030.ch>
2024-05-13 19:40:19 +0200

    add mofapy2-env and set MOFAPY2_PYTHON_PATH

--- flake.nix
+++ flake.nix
@@ -3,10 +3,17 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    mofapy2-env.url = "github:1pakch/mofapy2-env";
   };
 
-  outputs = { self, nixpkgs }: let
+  outputs = { self, nixpkgs, mofapy2-env }: let
+
     pkgs = nixpkgs.legacyPackages.x86_64-linux;
+
+    # Python env with `mofapy2` package available
+    py-env = mofapy2-env.packages.x86_64-linux.default;
+
+    # R env with `MOFA2` package and its R deps
     r-env = pkgs.rWrapper.override {
       packages = [
         pkgs.rPackages.IRkernel
@@ -14,9 +21,25 @@
         pkgs.rPackages.tidyverse
       ];
     };
+
+    # A shell script that launches the R as per above and
+    # additionally sets `MOFAPY2_PYTHON_PATH` env variable
+    # so that one can do
+    # ```
+    #   reticulate::use_python(Sys.getenv("MOFA2_PYTHON_PATH"))
+    #   run_mofa(...)
+    # ```
+    r-env-with-py-env = pkgs.writeShellScriptBin "R" ''
+      MOFAPY2_PYTHON_PATH="${py-env}/bin/python3" ${r-env}/bin/R
+    '';
+
     flakeApp = path: { type = "app"; program = path; };
+
   in {
-    apps.x86_64-linux.R = flakeApp "${r-env}/bin/R";
-    packages.x86_64-linux.default = r-env;
+
+    packages.x86_64-linux.default = r-env-with-py-env;
+
+    apps.x86_64-linux.default = (flakeApp "${r-env-with-py-env}/bin/R");
+
   };
 }
